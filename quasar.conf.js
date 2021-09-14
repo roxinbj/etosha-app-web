1cf9180b2683ca5d212d39277782ddc5a9a7bc66
Björn Roxin <nambro@posteo.de>
2021-09-14 12:44:52 +0200

    quasar config adjusted

--- quasar.conf.js
+++ quasar.conf.js
@@ -46,6 +46,7 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'history', // available values: 'hash', 'history'
+      publicPath: 'etosha-app-web',
 
       // transpile: false,
 
