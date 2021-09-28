b059e8180c0ecc7a6cb01d7ced40542fa30c8d59
roxinbj <nambro@posteo.de>
2021-09-28 12:03:01 +0200

    Fill forms (#2)
    * Added prerelease form
    
    * Added redirects
    
    * Rebuild
    
    * added netlifytoml
    
    * Try form in contactus
    
    * Added Contact us form subscribe

--- src/layouts/Prerelease.vue
+++ src/layouts/Prerelease.vue
@@ -7,9 +7,12 @@
         <p>Subscribe and be informed as soon as a release is out</p>
       </q-card-section>
       <q-card-section>
-        <form>
+        <form name="enter-subscribe" method="post" data-netlify="true" data-netlify-honeypot="bot-field" enctype="application/x-www-form-urlencoded" accept-charset="UTF-8">
+          <p style="display: none;" class="hidden"> 
+          <label> Dont fill it in <input name="form-name" value="enter-subscribe" type="hidden" /></label>
+          </p>
           <div class="input-wrapper">
-            <input type="email" placeholder="Email" name="name" required />
+            <input type="email" placeholder="Email" name="email" required />
             <button>Subscribe</button>
           </div>
         </form>
