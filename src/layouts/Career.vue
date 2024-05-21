44bc5bfedfc6a30a83c8a7bac31a3dd2a8b9829d
Björn Roxin <nambro@posteo.de>
2024-05-21 08:56:40 +0200

    "Deployment"

---
+++ src/layouts/Career.vue
@@ -0,0 +1,35 @@
+<template>
+  <Navbar />
+  <ContactForm />
+  <GetProductUpdates />
+  <Footer />
+</template>
+
+<script>
+import Navbar from "src/components/Navbar.vue";
+import Footer from "src/components/Footer.vue";
+import ContactForm from "src/subComponent/ContactForm.vue";
+import GetProductUpdates from "src/subComponent/GetProductUpdates.vue";
+import { useMeta } from "quasar";
+
+let metaData = {
+  // sets document title
+  title: "Etosha | Carrer",
+  // optional; sets final title as "Index Page - My Website", useful for multiple level meta
+  titleTemplate: (title) => `${title}`,
+  // meta tags
+  meta: {
+    description: { name: "theme-color", content: "#cb945e" },
+  },
+};
+
+export default {
+  components: { Navbar, Footer, ContactForm, GetProductUpdates },
+  setup() {
+    useMeta(metaData);
+  },
+  name: 'ContactUs'
+};
+</script>
+
+<style></style>
